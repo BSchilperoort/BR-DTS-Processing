171abc64df383b7192e105dc89f08f4863286dfb
BSchilperoort <b.schilperoort@gmail.com>
2016-04-08 14:17:43 +0200

    PEP-8 consistency

--- xml_conversion/xml_to_txt.py
+++ xml_conversion/xml_to_txt.py
@@ -14,8 +14,8 @@ ta = time()
 
 #Get all xml files from the directory
 #leave different channels in different directories!
-fileNames = sorted(glob(r'xml_conversion\xml_example\*.xml'))
-fileAmount = len(fileNames)
+file_names = sorted(glob(r'xml_conversion\xml_example\*.xml'))
+file_amount = len(file_names)
 
 #Initialise variables
 timestamp   = [None]
@@ -28,7 +28,7 @@ data_file = open(data_filename, 'w')
 data_file.write('Ultima data_file. Next row; distances (m).')
 
 #Get distances from xml
-tree = ET.parse(fileNames[0])
+tree = ET.parse(file_names[0])
 root = tree.getroot()
 data_line = []
 for measurement in root[0][11]:
@@ -39,12 +39,12 @@ data_length = len(distances)
 #Write distances to file
 data_file.write('\n'+';'.join(distances))
 
-#Write Time & Temp header
-data_file.write('\nTime\tTemp')
+#Write Time & temperature header
+data_file.write('\nTime\tTemperature')
 
 #Loop over all files and extract the 
-for ii in range(0,fileAmount):
-    tree = ET.parse(fileNames[ii])
+for ii in range(0,file_amount):
+    tree = ET.parse(file_names[ii])
     root = tree.getroot()
     
     #Copy timestamp from DTS to .txt
@@ -60,11 +60,11 @@ for ii in range(0,fileAmount):
         data_line[ii] = root[0][11][ii].text.split(',')
         
     #Extract temperature values from the dataline:
-    Temp = [row[3] for row in data_line]
+    temperature = [row[3] for row in data_line]
     
     #Append to file
-    fileLine = '\n'+timestamp+'\t'+';'.join(Temp)
-    data_file.write(fileLine)
+    file_line = '\n'+timestamp+'\t'+';'.join(temperature)
+    data_file.write(file_line)
 
 data_file.close()
 
